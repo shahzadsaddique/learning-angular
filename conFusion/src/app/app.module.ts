a6fba436759639dc5fed413dded73a142b1ea3aa
Shahzad Saddique <get.shahzadsaddique@gmail.com>
2020-12-05 13:12:36 +0000

    angular basic configurations

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
