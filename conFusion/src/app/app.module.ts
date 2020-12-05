d27ac3da2eeb08ef009d002f3a0ed9a45f5140cb
Shahzad Saddique <get.shahzadsaddique@gmail.com>
2020-12-05 12:47:40 +0000

    started working with angular

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
