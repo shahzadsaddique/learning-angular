5d55307189ae0b84579d6828f10963dfa9b65ccf
Shahzad Saddique <get.shahzadsaddique@gmail.com>
2020-12-06 11:10:12 +0000

    angular components part3

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -7,11 +7,13 @@ import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailsComponent } from './dish-details/dish-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailsComponent
   ],
   imports: [
     BrowserModule,
